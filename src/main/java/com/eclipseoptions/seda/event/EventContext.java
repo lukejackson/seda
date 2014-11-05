0e8ac10bb070bb27c6d90a061a294cc61f428991
Luke Jackson <lukejackson0@gmail.com>
2014-11-05 20:59:03 +0800

    clean event context

--- src/main/java/com/eclipseoptions/seda/event/EventContext.java
+++ src/main/java/com/eclipseoptions/seda/event/EventContext.java
@@ -1,11 +1,17 @@
 package com.eclipseoptions.seda.event;
 
+import com.eclipseoptions.seda.stage.Stage;
+import com.eclipseoptions.seda.transaction.Transaction;
+
 /**
- * Thread local context
- *
  * @author ljackson
  */
 public interface EventContext {
-    void reply(String requestId, Object response);
-    void publish(Object event);
+    Stage getStage();
+
+    String getDispatchCode();
+
+    Event getEvent();
+
+    Transaction getTransaction();
 }
--- src/main/java/com/eclipseoptions/seda/event/EventContextImpl.java
+++ src/main/java/com/eclipseoptions/seda/event/EventContextImpl.java
@@ -20,47 +20,23 @@ public class EventContextImpl implements EventContext {
         this.transaction = transaction;
     }
 
+    @Override
     public Stage getStage() {
         return stage;
     }
 
+    @Override
     public String getDispatchCode() {
         return dispatchCode;
     }
 
+    @Override
     public Event getEvent() {
         return event;
     }
 
+    @Override
     public Transaction getTransaction() {
         return transaction;
-    }
-
-    @Override
-    public <T> void reply(final T payload) {
-        // todo: move addition of txn resource to stage.send()
-        final Stage source = event.getSource();
-        if (source == null) {
-            throw new IllegalStateException("Cannot reply to event with no source. Event: " + event + ", reply: " + payload);
-        }
-        transaction.addResource(new TransactionResourceAdapter() {
-            @Override
-            public void postCommit() {
-                // todo: generate headers from payload
-                source.send(new DefaultEvent<Object>(stage, null, payload));
-            }
-        });
-    }
-
-    @Override
-    // todo: lookup event bus internally to avoid exposing to business logic?
-    public <T> void publish(final EventBus<T> eventBus, final T payload) {
-        transaction.addResource(new TransactionResourceAdapter() {
-            @Override
-            public void postCommit() {
-                // todo: generate headers from payload
-                eventBus.publish(new DefaultEvent<T>(null, null, payload));
-            }
-        });
     }
 }
